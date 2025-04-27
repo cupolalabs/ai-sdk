28e00590c926506829388ef0f24bebe68c78e733
Can Vardar <ismailcanvardar@gmail.com>
2025-04-28 02:02:53 +0300

    feat: implement serde to Entry

--- crates/providers/src/provider.rs
+++ crates/providers/src/provider.rs
@@ -1,35 +1,139 @@
 use crate::input_builder::Input;
+use serde::{Deserialize, Serialize};
 
-// -- tool choice starts here --
+// -- include field starts here --
+#[derive(Serialize, Deserialize)]
+enum Include {
+    #[serde(rename = "file_search_call.results")]
+    FileSearchCallResults,
+    #[serde(rename = "message.input_image.image_url")]
+    MessageInputImageUrl,
+    #[serde(rename = "computer_call_output.output.image_url")]
+    ComputerCallOutputImageUrl,
+}
+// -- include field ends here --
+
+// -- reasoning starts here --
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Effort {
+    Low,
+    Medium,
+    High,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Summary {
+    Auto,
+    Concise,
+    Detailed,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Reasoning {
+    effort: Option<Effort>,
+    summary: Option<Summary>,
+}
+// -- reasoning ends here --
+
+// -- service_tier field starts here --
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum ServiceTier {
+    Auto,
+    Default,
+    Flex,
+}
+// -- service_tier field ends here --
+
+// -- text field starts here --
+#[derive(Serialize, Deserialize)]
+#[serde(rename = "lowercase")]
+enum ResponseFormatType {
+    Text,
+    JsonSchema,
+    JsonObject,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TextFormat {
+    #[serde(rename = "type")]
+    type_field: ResponseFormatType, // always text
+}
+
+#[derive(Serialize, Deserialize)]
+struct JsonSchemaFormat {
+    #[serde(rename = "type")]
+    type_field: ResponseFormatType, // always jsonschema
+    name: String,
+    schema: serde_json::Value,
+    description: Option<String>,
+    strict: Option<bool>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct JsonObjectFormat {
+    #[serde(rename = "type")]
+    type_field: ResponseFormatType, // always jsonobject
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
+enum ResponseFormat {
+    Text(TextFormat),
+    JsonSchema(JsonSchemaFormat),
+    JsonObject(JsonObjectFormat),
+}
+
+#[derive(Serialize, Deserialize)]
+struct Text {
+    format: Option<ResponseFormat>,
+}
+// -- text field ends here --
+
+// -- tool_choice field starts here --
+#[derive(Serialize, Deserialize)]
+#[serde(rename = "lowercase")]
 enum ToolChoiceMode {
     None,
     Auto,
     Required,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(rename = "snake_case")]
 enum HostedToolType {
     FileSearch,
     WebSearchPreview,
     ComputerUsePreview,
 }
 
+#[derive(Serialize, Deserialize)]
 struct HostedTool {
+    #[serde(rename = "type")]
     type_field: HostedToolType,
 }
 
+#[derive(Serialize, Deserialize)]
 struct FunctionToolChoice {
     name: String,
+    #[serde(rename = "type")]
     type_field: String,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
 enum ToolChoice {
     Mode(ToolChoiceMode),
     HostedTool(HostedTool),
     FunctionTool(FunctionToolChoice),
 }
-// -- tool choice ends here --
+// -- tool_choice field ends here --
 
 // -- tool starts here --
+#[derive(Serialize, Deserialize)]
+#[serde(rename = "lowercase")]
 enum ComparisonOperator {
     Eq,
     Ne,
@@ -39,75 +143,104 @@ enum ComparisonOperator {
     Lte,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
 enum FilterValue {
     String(String),
     Number(f64),
     Boolean(bool),
 }
 
+#[derive(Serialize, Deserialize)]
 struct ComparisonFilter {
     key: String,
+    #[serde(rename = "type")]
     type_field: ComparisonOperator,
     value: FilterValue,
 }
 
+#[derive(Serialize, Deserialize)]
 enum CompoundOperationType {
     And,
     Or,
 }
 
-enum Filter {
+#[derive(Serialize, Deserialize)]
+struct CompoundFilter {
+    filters: Vec<FileSearchFilter>,
+    #[serde(rename = "type")]
+    type_field: CompoundOperationType,
+}
+
+#[derive(Serialize, Deserialize)]
+enum FileSearchFilter {
     Comparison(ComparisonFilter),
     Compound(CompoundFilter),
 }
 
-struct CompoundFilter {
-    filters: Vec<Filter>,
-    type_field: CompoundOperationType,
-}
-
+#[derive(Serialize, Deserialize)]
 struct RankingOptions {
     ranker: Option<String>,
     score_threshold: Option<f32>,
 }
 
+#[derive(Serialize, Deserialize)]
 struct FileSearchTool {
-    type_field: String,
+    #[serde(rename = "type")]
+    type_field: String, // NOTE: this is always "file_search" value
     vector_store_ids: Vec<String>,
-    filters: Option<Filter>,
+    filters: Option<FileSearchFilter>,
     max_num_results: Option<u8>,
     ranking_options: Option<RankingOptions>,
 }
 
+#[derive(Serialize, Deserialize)]
 struct FunctionTool {
     name: String,
     parameters: serde_json::Value,
     strict: bool,
-    type_field: String,
+    #[serde(rename = "type")]
+    type_field: String, // NOTE: this is always "function" value
     description: Option<String>,
 }
 
+#[derive(Serialize, Deserialize)]
 struct ComputerUseTool {
     display_height: f32,
     display_width: f32,
     environment: String,
-    type_field: String,
+    #[serde(rename = "type")]
+    type_field: String, // NOTE: this is always "computer_use_preview" value
 }
 
+#[derive(Serialize, Deserialize)]
 struct UserLocation {
-    type_field: String,
+    #[serde(rename = "type")]
+    type_field: String, // NOTE: this is always "approximate" value
     city: Option<String>,
-    country: Option<String>,
+    country: Option<String>, // NOTE: this is ISO-3166 country code
     region: Option<String>,
-    timezone: Option<String>,
+    timezone: Option<String>, // NOTE: this is IANA timezone
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(rename = "lowercase")]
+enum SearchContextSize {
+    Low,
+    Medium,
+    High,
+}
+
+#[derive(Serialize, Deserialize)]
 struct WebSearchTool {
-    type_field: String,
-    search_context_size: Option<String>,
+    #[serde(rename = "type")]
+    type_field: String, // NOTE: this is either web_search_preview or web_search_preview_2025_03_11C
+    search_context_size: Option<SearchContextSize>,
     user_location: Option<UserLocation>,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(untagged)]
 enum Tool {
     FileSearch(FileSearchTool),
     Function(FunctionTool),
@@ -116,77 +249,17 @@ enum Tool {
 }
 // -- tool ends here --
 
-// -- reasoning starts here --
-enum Effort {
-    Low,
-    Medium,
-    High,
-}
-
-enum Summary {
-    Auto,
-    Concise,
-    Detailed,
-}
-
-struct Reasoning {
-    effort: Option<Effort>,
-    summary: Option<Summary>,
-}
-// -- reasoning ends here --
-
+// -- trancation field starts here --
+#[derive(Serialize, Deserialize)]
+#[serde(rename = "losercase")]
 enum Truncation {
     Auto,
     Disabled,
 }
+// -- trancation field ends here --
 
-enum ServiceTier {
-    Auto,
-    Default,
-    Flex,
-}
-
-enum Include {
-    FileSearchCallResults,
-    MessageInputImageUrl,
-    ComputerCallOutputImageUrl,
-}
-
-// -- text starts here --
-enum ResponseFormatType {
-    Text,
-    JsonSchema,
-    JsonObject,
-}
-
-struct TextFormat {
-    type_field: ResponseFormatType, // always text
-}
-
-struct JsonSchemaFormat {
-    type_field: ResponseFormatType, // always jsonschema
-    name: String,
-    schema: serde_json::Value,
-    description: Option<String>,
-    strict: Option<bool>,
-}
-
-struct JsonObjectFormat {
-    type_field: ResponseFormatType, // always jsonobject
-}
-
-enum ResponseFormat {
-    Text(TextFormat),
-    JsonSchema(JsonSchemaFormat),
-    JsonObject(JsonObjectFormat),
-}
-
-struct Text {
-    format: Option<ResponseFormat>,
-}
-// -- text ends here --
-
-struct Request<'a> {
+#[derive(Serialize, Deserialize)]
+pub struct Entry<'a> {
     input: Input<'a>,
     include: Option<Vec<Include>>,
     instructions: Option<&'a str>,
