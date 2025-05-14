d5218a71e3165096b1570791107d83c9be559125
Can Vardar <ismailcanvardar@gmail.com>
2025-05-14 13:58:25 +0300

    test(common): add test suite

--- crates/providers/src/request/input/common.rs
+++ crates/providers/src/request/input/common.rs
@@ -78,12 +78,23 @@ pub struct TextContent<'a> {
     pub text: &'a str,
 }
 
-impl<'a> TextContent<'a> {
-    pub fn new(text: &'a str) -> Self {
+impl Default for TextContent<'_> {
+    fn default() -> Self {
         Self {
             type_field: ContentType::InputText,
-            text,
+            text: "",
         }
+    }
+}
+
+impl<'a> TextContent<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn text(mut self, text: &'a str) -> Self {
+        self.text = text;
+        self
     }
 }
 
@@ -222,5 +233,127 @@ impl<'a> From<ImageContent<'a>> for Content<'a> {
 impl<'a> From<FileContent<'a>> for Content<'a> {
     fn from(file_content: FileContent<'a>) -> Self {
         Self::File(file_content)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    // let's compare the json output of the default values
+    #[test]
+    fn test_default_values() {
+        let text_content = TextContent::default();
+        let image_content = ImageContent::default();
+        let file_content = FileContent::default();
+
+        let text_content_json = serde_json::to_value(&text_content).unwrap();
+        let image_content_json = serde_json::to_value(&image_content).unwrap();
+        let file_content_json = serde_json::to_value(&file_content).unwrap();
+
+        assert_eq!(text_content_json, json!({"type": "input_text", "text": ""}));
+        assert_eq!(
+            image_content_json,
+            json!({"type": "input_image", "detail": "auto"})
+        );
+        assert_eq!(file_content_json, json!({"type": "input_file"}));
+    }
+
+    #[test]
+    fn test_text_content() {
+        let text = "Hello, world!";
+        let text_content = TextContent::new().text(text);
+        let text_content_json = serde_json::to_value(&text_content).unwrap();
+        assert_eq!(
+            text_content_json,
+            json!({"type": "input_text", "text": text})
+        );
+    }
+
+    #[test]
+    fn test_image_content() {
+        let image_url = "https://example.com/image.png";
+        let file_id = "1234567890";
+        let detail = "auto";
+
+        let image_content = ImageContent::new()
+            .image_url(image_url)
+            .file_id(file_id)
+            .detail(detail)
+            .unwrap();
+
+        let image_content_json = serde_json::to_value(&image_content).unwrap();
+        assert_eq!(
+            image_content_json,
+            json!({"type": "input_image", "image_url": image_url, "file_id": file_id, "detail": detail})
+        );
+    }
+
+    #[test]
+    fn test_file_content() {
+        let file_id = "1234567890";
+        let file_data = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=";
+        let filename = "image.png";
+
+        let file_content = FileContent::new()
+            .file_id(file_id)
+            .file_data(file_data)
+            .filename(filename);
+
+        let file_content_json = serde_json::to_value(&file_content).unwrap();
+        assert_eq!(
+            file_content_json,
+            json!({"type": "input_file", "file_id": file_id, "file_data": file_data, "filename": filename})
+        );
+    }
+
+    #[test]
+    fn test_status_from_str() {
+        assert_eq!(Status::from_str("in_progress").unwrap(), Status::InProgress);
+        assert_eq!(Status::from_str("completed").unwrap(), Status::Completed);
+        assert_eq!(Status::from_str("incomplete").unwrap(), Status::Incomplete);
+        assert_eq!(Status::from_str("failed").unwrap(), Status::Failed);
+    }
+
+    #[test]
+    fn test_role_from_str() {
+        assert_eq!(Role::from_str("user").unwrap(), Role::User);
+        assert_eq!(Role::from_str("assistant").unwrap(), Role::Assistant);
+        assert_eq!(Role::from_str("system").unwrap(), Role::System);
+        assert_eq!(Role::from_str("developer").unwrap(), Role::Developer);
+    }
+
+    #[test]
+    fn test_image_detail_from_str() {
+        assert_eq!(ImageDetail::from_str("high").unwrap(), ImageDetail::High);
+        assert_eq!(ImageDetail::from_str("low").unwrap(), ImageDetail::Low);
+        assert_eq!(ImageDetail::from_str("auto").unwrap(), ImageDetail::Auto);
+    }
+
+    #[test]
+    fn test_from_specific_content_to_content() {
+        let text = "Hello, world!";
+        let image_url = "https://example.com/image.png";
+        let file_id = "1234567890";
+
+        let text_content_builder = TextContent::new().text(text);
+        let text_content: Content = text_content_builder.into();
+
+        let image_content_builder = ImageContent::new().image_url(image_url);
+        let image_content: Content = image_content_builder.into();
+
+        let file_content_builder = FileContent::new().file_id(file_id);
+        let file_content: Content = file_content_builder.into();
+
+        assert_eq!(text_content, Content::Text(TextContent::new().text(text)));
+        assert_eq!(
+            image_content,
+            Content::Image(ImageContent::new().image_url(image_url))
+        );
+        assert_eq!(
+            file_content,
+            Content::File(FileContent::new().file_id(file_id))
+        );
     }
 }
